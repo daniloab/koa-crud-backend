fbdd463d9ed94df83a3ee372245cc870a4aed348
daniloab <daniloassis.ti@gmail.com>
2021-05-25 23:56:50 -0300

    feat(mongoose): creating connection

--- src/config.ts
+++ src/config.ts
@@ -13,4 +13,5 @@ dotenvSafe.config({
 export const config = {
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV,
+  MONGO_URI: process.env.MONGO_URI,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import 'isomorphic-fetch';
-import { createServer } from 'http';
-import app from './app';
-import { config } from './config';
-// import { connectDatabase } from './mongo';
+import "isomorphic-fetch";
+import { createServer } from "http";
+import app from "./app";
+import { config } from "./config";
+import { connectDatabase } from "./mongo";
 
 (async () => {
-    // await connectDatabase();
+  await connectDatabase();
 
-    const server = createServer(app.callback());
+  const server = createServer(app.callback());
 
-    server.listen(config.PORT, () => {
-        console.log(`server running at http://localhost:${config.PORT}`);
-    });
+  server.listen(config.PORT, () => {
+    console.log(`server running at http://localhost:${config.PORT}`);
+  });
 })();
