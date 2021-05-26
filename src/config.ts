ccfc221c699a4a8a039d1cb3017b56602d2229ac
daniloab <daniloassis.ti@gmail.com>
2021-05-25 23:44:59 -0300

    chore(env): updating envs

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
-import dotenvSafe from 'dotenv-safe';
-import path from 'path';
+import dotenvSafe from "dotenv-safe";
+import path from "path";
 
 const cwd = process.cwd();
 
 const root = path.join.bind(cwd);
 
 dotenvSafe.config({
-    path: root('.env'),
-    sample: root('.env.example'),
+  path: root(".env"),
+  sample: root(".env.example"),
 });
 
 export const config = {
-    PORT: process.env.PORT || 6000,
-    NODE_ENV: process.env.NODE_ENV,
-}
+  PORT: process.env.PORT || 5000,
+  NODE_ENV: process.env.NODE_ENV,
+};
